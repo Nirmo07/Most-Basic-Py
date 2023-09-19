5018e2e5dc403b46a824f167622ab159ba87a792
Nirmo07 <132211977+Nirmo07@users.noreply.github.com>
2023-09-19 20:15:13 +0530

    Update 5.py

--- hello world/5.py
+++ hello world/5.py
@@ -1,1 +1,9 @@
-
+#Letter to myself😊
+print("                                          19-09-2023")
+print("Hello! dear Nirmohi this is me from past i hope u   ")
+print("have archieved many things in ur life. i hope that ")
+print("that u r leaving ur life to the fulliest.")
+print("srry if i have done any mistakes.but smile everytime.")
+print("from ur past")
+print("ur dear ")
+print("Nirmohi (╥﹏╥)")
