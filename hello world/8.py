892293528a916d1c25c2b67644d16c40c31664be
Nirmo07 <132211977+Nirmo07@users.noreply.github.com>
2023-10-04 16:33:55 +0530

    Update 8.py

--- hello world/8.py
+++ hello world/8.py
@@ -1,1 +1,15 @@
 
+score = 2 ** 2      # score is 4
+score = 2 ** 3      # score is now 8
+score = 2 ** 4      # score is now 16
+score = 2 ** 5      # score is now 32
+
+print(score)        # Output: 32
+
+
+height=159**2
+mass=55
+bmi=mass/height
+print('height=159')
+print('bmi:',bmi)
+
