c0bd47c5b5a589942260c063396573b583e68756
Nirmo07 <132211977+Nirmo07@users.noreply.github.com>
2023-10-04 17:33:10 +0530

    Delete hello world/10.py

--- hello world/10.py
+++
@@ -1,6 +0,0 @@
-# Write code below 💖
-a=int(input('what do you have left in pesos?'))
-b=int(input('what do you have left in soles?'))
-c=int(input('what do you have left in reais?'))
-d=a*0.056+b*0.26+c*0.19
-print('USD:',d)
